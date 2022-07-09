d7cb047743bb9c25638fd4537565bffc8127ec60
Mohamed Ebrahim <ebrahimm131@gmail.com>
2022-07-09 23:43:11 +0400

    organize classes

---
+++ app/src/main/java/com/example/lemonade/LemonTree.kt
@@ -0,0 +1,13 @@
+package com.example.lemonade
+
+
+
+/**
+ * A Lemon tree class with a method to "pick" a lemon. The "size" of the lemon is randomized
+ * and determines how many times a lemon needs to be squeezed before you get lemonade.
+ */
+class LemonTree {
+    fun pick(): Int {
+        return (2..4).random()
+    }
+}
--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -147,12 +147,4 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-/**
- * A Lemon tree class with a method to "pick" a lemon. The "size" of the lemon is randomized
- * and determines how many times a lemon needs to be squeezed before you get lemonade.
- */
-class LemonTree {
-    fun pick(): Int {
-        return (2..4).random()
-    }
-}
+
